2c502ccb0a1ff124ea0f35b5b8d7a1082b4c87b3
Hofer Lukas <csaz9699@student.uibk.ac.at>
2025-03-18 10:51:38 +0100

    implement addNewNote

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useState, useEffect } from 'react'
 import axios from 'axios'
+
+import { useState, useEffect } from 'react'
 import { Typography } from '@mui/material'
 
 import Box from '@mui/material/Box';
@@ -8,6 +9,9 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+
 
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
@@ -16,6 +20,17 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 function App() {
     const [notes, setNotes] = useState([]);
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [openModalAddNote, setOpenModalAddNote] = React.useState(false);
+    const handleOpenModalAddNote = () => setOpenModalAddNote(true);
+    const handleCloseModalAddNote = () => {
+        setOpenModalAddNote(false);
+        setTitle('');
+        setContent('');
+    };
 
     const fetchNotes = () => {
         axios
@@ -47,9 +62,42 @@ function App() {
         }
     };
 
+    const addNote = () => {
+        if (title === '') {
+            console.log('title is empty');
+            return
+        } else {
+            axios
+                .post(`/api/notes`, {
+                    title: title,
+                    content: content,
+                })
+                .then(() => {
+                    fetchNotes();
+                    handleCloseModalAddNote();
+                    console.log('note added');
+                })
+                .catch((error) => {
+                    console.error("Error adding notification:", error);
+                });
+        }
+    };
+
     useEffect(() => {
         fetchNotes();
     }, []);
+
+    const modalStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
 
     return (
@@ -58,7 +106,7 @@ function App() {
                 <Typography variant="h3" gutterBottom>
                     My Notes:
                 </Typography>
-                <Button variant="contained">
+                <Button variant="contained" onClick={handleOpenModalAddNote}>
                     Add Note
                     <AddCircleOutlineIcon fontSize="small" sx={{ ml: 1 }} />
                 </Button>
@@ -87,6 +135,44 @@ function App() {
                     </Card>
                 ))}
             </Box>
+
+
+            <Modal
+                open={openModalAddNote}
+                onClose={handleCloseModalAddNote}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={{
+                    ...modalStyle,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2
+                }}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Write your note:
+                    </Typography>
+                    <TextField
+                        id="standard-basic"
+                        label="Title:"
+                        variant="standard"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <TextField
+                        id="standard-multiline-static"
+                        label="Content:"
+                        multiline
+                        rows={4}
+                        variant="standard"
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+                    <Box sx={{ display: 'flex', gap: 2, mt: 2 }}>
+                        <Button onClick={addNote}>Save</Button>
+                        <Button onClick={handleCloseModalAddNote}>Close</Button>
+                    </Box>
+                </Box>
+            </Modal>
+
         </div>
     )
 }
