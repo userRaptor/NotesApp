f6fcae2798d129c62b86997f7cb732694058cb2b
Hofer Lukas <csaz9699@student.uibk.ac.at>
2025-03-18 09:39:04 +0100

    implement basic Card (Material UI)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Typography } from '@mui/material'
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+
+
+
 
 function App() {
     const [notes, setNotes] = useState([]);
@@ -8,7 +22,7 @@ function App() {
         axios
             .get(`/api/notes`)
             .then((response) => {
-                console.log(response.data); 
+                console.log(response.data);
                 setNotes(response.data);
             })
             .catch((error) => {
@@ -21,17 +35,46 @@ function App() {
     }, []);
 
 
-
     return (
         <div>
-            <h2>Alle Notizen</h2>
+            <Typography variant="h3" gutterBottom>
+                Meine Notizen:
+            </Typography>
+
             {notes.map(note => (
-                <div key={note.id}>
+                <div key={note.noteId}>
                     <h3>{note.title}</h3>
                     <p>{note.content}</p>
-                    <button onClick={() => handleDelete(note.id)}>Löschen</button>
+                    <button onClick={() => handleDelete(note.noteId)}>Löschen</button>
                 </div>
             ))}
+
+
+
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+                    </Button>
+                </CardActions>
+            </Card>
 
         </div>
     )
